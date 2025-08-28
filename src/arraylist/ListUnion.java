dd0983c747ed39516f20d506a95a88ec8e97ad4b
Gaurav-303 <gaurav134mishra@gmail.com>
2025-08-28 15:32:13 +0530

    union of two different list

---
+++ src/arraylist/ListUnion.java
@@ -0,0 +1,43 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class ListUnion {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(10);
+        list1.add(20);
+        list1.add(30);
+        list1.add(40);
+
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(30);
+        list2.add(40);
+        list2.add(50);
+        list2.add(60);
+
+        ArrayList<Integer> union = new ArrayList<>();
+
+
+        for (Integer num : list1) {
+            if (!union.contains(num)) {
+                union.add(num);
+            }
+        }
+
+
+        for (Integer num : list2) {
+            if (!union.contains(num)) {
+                union.add(num);
+            }
+        }
+
+
+        System.out.println("List 1: " + list1);
+        System.out.println("List 2: " + list2);
+        System.out.println("Union: " + union);
+    }
+}
+
